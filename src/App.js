325100fa72b062da89a9cdfa531476c29dd28625
naufallafif <naufalafif2207@gmail.com>
2024-10-07 11:37:19 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const ProductCart = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cards">
+      <img style={{
+        width: '100%',
+        height: "200px",
+        borderRadius: "10px 30px 0px 0px",
+      }}
+        src='https://images.unsplash.com/photo-1551830820-330a71b99659?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4MTQ2MDgwOQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080'
+        alt=''
+      />
+      <div className="container">
+        <h4>
+          <b>ford</b>
+        </h4>
+        <p>Mobil Ford 2010</p>
+      </div>
     </div>
   );
-}
+};
+
+function App() {
+  return <div className='cards'>
+    <ProductCart/>
+  </div>
+};
 
 export default App;
